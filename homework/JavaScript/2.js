a345f27a45ad403cadd3f36deb36a087f1e2210e
peiyun328 <99934914+peiyun328@users.noreply.github.com>
2022-06-06 12:48:54 +0800

    Update 2.js

--- homework/JavaScript/2.js
+++ homework/JavaScript/2.js
@@ -7,7 +7,7 @@ function fac(num){
     }
     return res
 }
-//console.log("8! = ","8*7*6*5*4*3*2*1 = ",fac(8))
+//console.log("8! = ",fac(8))
 
 var num = parseInt(Deno.args[0])
 console.log('factorial(', num, ')=', fac(num))
