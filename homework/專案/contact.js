3e6265564b8d9f040b3a4ddfb90e6d9207e5482a
peiyun328 <99934914+peiyun328@users.noreply.github.com>
2022-06-15 23:16:58 +0800

    Update contact.js

--- homework/專案/contact.js
+++ homework/專案/contact.js
@@ -4,11 +4,11 @@ var submitBtn = document.querySelector('.submit')
 function submitheck() {
   var nameStr = document.querySelector('.name').value
   var emailStr = document.querySelector('.email').value
-  var passwordStr = document.querySelector('.password').value
+  var phoneStr = document.querySelector('.phone').value
   var account = {} //輸入的資料，填入空物件
   account.nameStr = nameStr //填入的姓名
   account.email = emailStr //填入的 email
-  account.password = passwordStr //填入的密碼
+  account.phone = phoneStr //填入的密碼
 
   var sub = new XMLHttpRequest()
   sub.open('post', 'https://hexschool-tutorial.herokuapp.com/api/signup', true)
