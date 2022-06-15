6785bc18081ee101d7775034b9bc746ac8bb4feb
peiyun328 <99934914+peiyun328@users.noreply.github.com>
2022-06-15 23:36:58 +0800

    Update contact.js

--- homework/專案/contact.js
+++ homework/專案/contact.js
@@ -6,7 +6,7 @@ function submitheck() {
   var emailStr = document.querySelector('.email').value
   var phoneStr = document.querySelector('.phone').value
   var account = {} //輸入的資料，填入空物件
-  account.nameStr = nameStr //填入的姓名
+  account.name = nameStr //填入的姓名
   account.email = emailStr //填入的 email
   account.phone = phoneStr //填入的密碼
 
@@ -16,7 +16,12 @@ function submitheck() {
   var data = JSON.stringify(account)
   sub.send(data)
   sub.onload = function () {
-    alert("提交成功")
+      if(account.email!='' && account.name!='' && account.phone!=''){
+        alert("提交成功")
+      }
+      else{
+          alert("請完成空白處")
+      }
   }
 }
 submitBtn.addEventListener('click', submitheck, false)
